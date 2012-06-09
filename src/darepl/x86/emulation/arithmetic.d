27ad0a3943ba0fddfecab7c10b9349698e5594a1
alexrp <alex@lycus.org>
2012-06-09 03:15:06 +0200

    Add some x86 instruction emulation functions.

---
+++ src/darepl/x86/emulation/arithmetic.d
@@ -0,0 +1,18 @@
+module darepl.x86.emulation.arithmetic;
+
+import darepl.core.lexer,
+       darepl.x86.instructions,
+       darepl.x86.machine;
+
+public extern (C)
+{
+    void x86_add_reg32_reg32(X86Machine machine, X86Instruction instruction, X86Register32 dst, X86Register32 src)
+    {
+        dst.memory.u32[0] += src.memory.u32[0];
+    }
+
+    void x86_add_reg32_int32(X86Machine machine, X86Instruction instruction, X86Register32 dst, Literal imm)
+    {
+        dst.memory.u32[0] += imm.value.intValue.value32u;
+    }
+}
---
+++ src/darepl/x86/emulation/misc.d
@@ -0,0 +1,11 @@
+module darepl.x86.emulation.misc;
+
+import darepl.x86.instructions,
+       darepl.x86.machine;
+
+public extern (C)
+{
+    void x86_nop(X86Machine machine, X86Instruction instruction)
+    {
+    }
+}
